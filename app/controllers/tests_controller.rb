3f1b604164a7c23ad608fc9c7aea3bbe2319e3a2
dghinato <dghinato@gmail.com>
2014-02-14 18:32:36 +0100

    Fatte alcune validation, una prima bozza della validazione di test_question.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,28 @@
 class TestsController < InheritedResources::Base
+  
+  MAX = 3
+  
   def create
-   test = Test.create(permitted_params_con_select)
-   questions = Question.find(params[:question_id_1], params[:question_id_2], params[:question_id_3])
-
+   
+   #test = Test.create(permitted_params_con_select)
+   #questions = Question.find(params[:question_id_1], params[:question_id_2], params[:question_id_3])
+   
+=begin  
    questions.each do |question|
      question.test_questions.create(test_id: test.id)   
-    end
+   end
+=end
+
+   test = Test.new(permitted_params_con_select)
+
+   for i in 1..MAX
+     question = Question.find(params["question_id_" + i.to_s])
+     test_question = TestQuestion.new
+     test_question.question = question 
+     test_question.test = test
+     test_question.save!
+   end
+   test.save!
 
    #redirect_to test_questions_path
    redirect_to tests_path
